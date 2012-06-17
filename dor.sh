16b7566731a63ff4272b6c3aec6ed0e453a8406a
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2012-06-17 10:09:52 -0700

    First pass at getting Omnidroid runs working.

---
+++ dor.sh
@@ -0,0 +1,6 @@
+export LUA_PATH="./?.lua;/usr/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/init.lua;/usr/lib/lua/5.1/?.lua;/usr/lib/lua/5.1/?/init.lua;/usr/local/share/lua/5.1/?.lua;/usr/share/lua/5.1/?/init.lua"
+
+export PATH="${PATH}:~/util"
+
+cd /cygdrive/c/werk/immortals
+colorize ./Den
