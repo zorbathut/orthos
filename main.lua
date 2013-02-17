8cb87af246ac697b86f171e4f016583c4b9004f4
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2013-02-17 07:10:36 -0800

    fix environment creation, move menu into the real file

--- main.lua
+++
@@ -1,6 +0,0 @@
-
-function tick_loop(...)
-end
-function key()
-end
--- there's more that aren't commonly used
---
+++ mainmenu.lua
@@ -0,0 +1,135 @@
+
+do
+  -- params
+  local width = 0.7
+  local starpos = (1 - width) / 2
+  
+  local step = width / 5
+  
+  local descale = 0.9
+  local solid_intensity = 1
+  
+  local idx = 0
+  local gadgetwidth
+  for c in ("ORTHOS"):gmatch(".") do
+    local char
+    if idx == 4 then
+      local rwidth = gadgetwidth * descale
+      
+      char = Frames.Raw(Frames.Root)
+      char:SetWidth(gadgetwidth)
+      char:SetHeight(gadgetwidth)
+      local timer = math.random() * 360
+      local lasttime = Inspect.System.Time.Real()
+      char:EventRenderAttach(function ()
+        gl.Disable("DEPTH_TEST")
+        gl.DepthFunc("LEQUAL")
+        gl.DepthMask(true)
+        gl.BlendFunc("ONE", "ONE")
+        gl.MatrixMode("MODELVIEW")
+        gl.Translate((char:GetLeft() + char:GetRight()) / 2, (char:GetTop() + char:GetBottom()) / 2, 0)
+        gl.Scale(rwidth / 2, rwidth / 2, 0.5)
+        gl.Rotate(math.sin(timer) * 10, 1, 0, 0)
+        gl.Rotate(timer * 4, 0, 0, 1)
+        gl.Rotate(35.264, 1, 0, 0)
+        gl.Rotate(45, 0, 1, 0)
+        
+        timer = timer + (Inspect.System.Time.Real() - lasttime) * 0.3
+        lasttime = Inspect.System.Time.Real()
+        
+        gl.Begin("QUADS")
+        gl.Color(solid_intensity, 0, 0)
+        gl.Vertex(-1, -1, 0)
+        gl.Vertex(-1, 1, 0)
+        gl.Vertex(1, 1, 0)
+        gl.Vertex(1, -1, 0)
+        gl.Color(0, solid_intensity, 0)
+        gl.Vertex(-1, 0, -1)
+        gl.Vertex(-1, 0, 1)
+        gl.Vertex(1, 0, 1)
+        gl.Vertex(1, 0, -1)
+        gl.Color(0, 0, solid_intensity)
+        gl.Vertex(0, -1, -1)
+        gl.Vertex(0, -1, 1)
+        gl.Vertex(0, 1, 1)
+        gl.Vertex(0, 1, -1)
+        gl.End()
+      end)
+    else
+      char = Frames.Text(Frames.Root)
+      char:SetText(c)
+      char:SetFont("font/FundamentalBrigade.ttf")
+      char:SetSize(300)
+      
+      if idx == 0 then
+        gadgetwidth = char:GetWidth()
+      end
+    end
+    
+    char:SetPoint("CENTER", Frames.Root, starpos + step * idx, 0.25)
+    idx = idx + 1
+  end
+  
+  local function MakeRandomBg(restrict)
+    local tex = Frames.Texture(Frames.Root)
+    tex:SetLayer(-1)
+    
+    local opts = {"astral", "city", "cyber"}
+    local chose
+    while not chose do
+      chose = opts[math.random(#opts)]
+      if chose == restrict then chose = nil end
+    end
+    local texit = string.format("copyright_infringement/%s%02d.jpg", chose, math.random(3))
+    tex:SetTexture(texit)
+    
+    local cols = {
+      astral = {1, 0, 0},
+      city = {0, 1, 0},
+      cyber = {0, 0, 1}
+    }
+    tex:SetTint(unpack(cols[chose]))
+    
+    tex:SetPoint("CENTER", Frames.Root, "CENTER")
+    local scal = math.min(Frames.Root:GetHeight() / tex:GetHeight(), Frames.Root:GetWidth() / tex:GetWidth())
+    tex:SetHeight(tex:GetHeight() * scal)
+    tex:SetWidth(tex:GetWidth() * scal)
+    
+    return tex, chose
+  end
+  
+  local delay = 2
+  local fade = 3
+  Event.System.Update.Begin:Attach(
+    coroutine.wrap(
+      function ()
+        local bga, cat = MakeRandomBg("")
+        local creation = Inspect.System.Time.Real()
+        
+        while true do
+          while Inspect.System.Time.Real() < creation + delay do
+            coroutine.yield()
+          end
+          
+          local nbga, ncat = MakeRandomBg(cat)
+          bga:SetLayer(-1)
+          nbga:SetAlpha(0)
+          
+          while Inspect.System.Time.Real() < creation + delay + fade do
+            local shift = (Inspect.System.Time.Real() - creation - delay) / fade
+            bga:SetAlpha(1 - shift)
+            nbga:SetAlpha(shift)
+            coroutine.yield()
+          end
+          
+          bga:Obliterate()
+          
+          bga, cat = nbga, ncat
+          creation = creation + delay + fade
+          bga:SetAlpha(1)
+        end
+      end
+    )
+  )
+end
+
