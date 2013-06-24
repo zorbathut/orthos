d4576b29f1463bb579dea2edc4cb849349b88c60
Ben Wilhelm <zorba-git20110501@pavlovian.net>
2013-06-24 03:07:36 -0700

    Make the basic spike SFX look good-ish

--- battle_ability.lua
+++ battle_ability.lua
@@ -1,4 +1,7 @@
 
+local sfx = Frame.Frame(Frame.Root)
+sfx:SetLayer(100)
+
 print("BAB")
 
 local lookup = {
@@ -6,6 +9,8 @@ local lookup = {
     local ix, iy = initiator.x, initiator.y
     local grid = Inspect.Battle.Grid()
     local entities = Inspect.Battle.Entities()
+    
+    Command.Battle.Cast("SFXBlast", ix, iy);
     
     local closelen = 1000
     local closeent = nil
@@ -21,15 +26,42 @@ local lookup = {
     
     if closeent then
       closeent:Hit()
+      Command.Battle.Cast("SFXExplosion", closeent);
     end
-  end
+  end,
+  
+  SFXBlast = function (x, y)
+    local grid = Inspect.Battle.Grid()
+    local blaster = Frame.Texture(sfx)
+    blaster:SetTexture("placeholder/blaster.png")
+    blaster:SetPoint("CENTERLEFT", grid[x][y], "CENTER", 20, -20)
+    
+    Command.Coro.Wait(0.1)
+    
+    blaster:Obliterate()
+  end,
+  
+  SFXExplosion = function (target)
+    local grid = Inspect.Battle.Grid()
+    local explosion = Frame.Texture(sfx)
+    explosion:SetTexture("copyright_infringement/Explosion.png")
+    explosion:SetPoint("CENTER", grid[target.x][target.y], "CENTER", 0, 0)
+    
+    local dur = 60 * 0.3
+    for k = 1, dur do
+      coroutine.yield()
+      explosion:SetTint(1, 1, 1, 1 - k / dur)
+    end
+    
+    explosion:Obliterate()
+  end,
 }
 
 local actives = {}
 
-Command.Environment.Insert(_G, "Command.Battle.Cast", function (abilityId, initiatingEntity)
-  print("Casting", abilityId, initiatingEntity)
-  actives[coroutine.spawn(function (...) lookup[abilityId](...) while true do coroutine.yield(true) end end, initiatingEntity)] = true
+Command.Environment.Insert(_G, "Command.Battle.Cast", function (abilityId, ...)
+  print("Casting", abilityId, ...)
+  actives[coroutine.spawn(function (...) lookup[abilityId](...) while true do coroutine.yield(true) end end, ...)] = true
 end)
 
 print("CBC", Command.Battle.Cast)
